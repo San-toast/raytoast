b580bdc45fe28ebc20196dcc795730bffb855003
Santos <sg21894@gmail.com>
2022-03-11 10:11:05 -1000

    Server Setup

---
+++ index.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+var pg = require("pg");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT;
+app.use(express.json());
+//or native libpq bindings
+//var pg = require('pg').native
+
+var conString = process.env.conString;
+var client = new pg.Client(conString);
+client.connect(function (err) {
+  if (err) {
+    return console.error("could not connect to postgres", err);
+  }
+  client.query('SELECT NOW() AS "theTime"', function (err, result) {
+    if (err) {
+      return console.error("error running query", err);
+    }
+    console.log(result.rows[0].theTime);
+    // >> output: 2018-08-23T14:02:57.117Z
+    client.end();
+  });
+});
+
+app.get("/hello", (req, res) => {
+  res.send("Hello");
+});
+app.listen(PORT, console.log(`running on port ${PORT}`));
