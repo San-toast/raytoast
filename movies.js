09568cb7eba2588e047be000b5c19bbf2f37fd69
Rayleigh Rozier <rayleighrozier@gmail.com>
2022-03-11 20:39:10 -0500

    get now playing titles

--- movies.js
+++ movies.js
@@ -1,12 +1,17 @@
 require("dotenv").config();
 const apiKey = process.env.apiKey;
 const fetch = require("node-fetch");
-const getMovies = async () => {
-  let url = `http://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}`;
-  const movie = await fetch(url, {
+const getNowPlaying = async () => {
+  let url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=1`;
+  const nowPlaying = await fetch(url, {
     method: "GET",
   });
-  let movieJson = await movie.json();
-  console.log(movieJson);
+  let nowPlayingJson = await nowPlaying.json();
+  //   console.log(movieJson.results);
+  let titles = [];
+  for (movie of nowPlayingJson.results) {
+    titles.push(movie.title);
+  }
+  console.log(titles);
 };
-getMovies();
+getNowPlaying();
