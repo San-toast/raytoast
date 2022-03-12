da9798feef990917a3a6d0cc8084391c3bbf7795
Rayleigh Rozier <rayleighrozier@gmail.com>
2022-03-11 20:58:54 -0500

    formatted now playing movies

--- movies.js
+++ movies.js
@@ -1,17 +1,21 @@
 require("dotenv").config();
 const apiKey = process.env.apiKey;
 const fetch = require("node-fetch");
+let nowPlaying = [];
 const getNowPlaying = async () => {
   let url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=1`;
-  const nowPlaying = await fetch(url, {
+  const movies = await fetch(url, {
     method: "GET",
   });
-  let nowPlayingJson = await nowPlaying.json();
-  //   console.log(movieJson.results);
-  let titles = [];
-  for (movie of nowPlayingJson.results) {
-    titles.push(movie.title);
+  let moviesJson = await movies.json();
+  for (i = 0; i < moviesJson.results.length; i++) {
+    nowPlaying[i] = {
+      title: moviesJson.results[i].title,
+      id: moviesJson.results[i].id,
+      backdrop: `https://image.tmdb.org/t/p/w1280/${moviesJson.results[i].backdrop_path}`,
+      poster: `https://image.tmdb.org/t/p/w780/${moviesJson.results[i].poster_path}`,
+    };
   }
-  console.log(titles);
+  console.log(nowPlaying);
 };
 getNowPlaying();
