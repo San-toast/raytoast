671a73ca8bbb49dcce9958d23516aab352aaf67f
Santos <sg21894@gmail.com>
2022-03-14 08:30:11 -1000

    created lists on concessions page

--- routes/concessions.js
+++ routes/concessions.js
@@ -5,8 +5,10 @@ const db = require("../models");
 router.get("/:movieid/:showtimeid/concessions", async (req, res) => {
   let movieid = req.params.movieid;
   let showtimeid = req.params.showtimeid;
-  res.render("pages/concessions");
   let concessions = await db.Concessions.findAll();
+  res.render("pages/concessions", {
+    concessions: concessions,
+  });
   console.log(concessions[0].dataValues);
 });
 
