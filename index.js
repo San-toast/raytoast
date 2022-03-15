f19af9d6fe85d4adeef7e6601a320fa633b0e63d
Santos <sg21894@gmail.com>
2022-03-15 10:11:31 -1000

    created page to view tickets

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ client.connect(function (err) {
 
 //routes
 app.use("/", home);
-app.use("/tickets", tickets);
+app.use("/", tickets);
 app.use("/", reserve);
 app.use("/", showtimes);
 app.use("/", concessions);
--- routes/tickets.js
+++ routes/tickets.js
@@ -11,4 +11,11 @@ router.get("/get/:id", async (req, res) => {
   let getShowtime = await db.Tickets.findAll({ where: { showtimeId: id } });
   res.send(getShowtime);
 });
+
+router.get("/theatre", async (req, res) => {
+  let tickets = await db.Tickets.findAll();
+  res.render("pages/theatre", {
+    tickets: tickets,
+  });
+});
 module.exports = router;
