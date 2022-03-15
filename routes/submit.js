7917717da7aa948f4bc11036661a38fa518962ab
Santos <sg21894@gmail.com>
2022-03-15 11:17:36 -1000

    put info in submit page, created table for tickets

--- routes/submit.js
+++ routes/submit.js
@@ -3,9 +3,15 @@ const router = express.Router();
 const db = require("../models");
 
 router.get("/submit", async (req, res) => {
+  let latest = await db.Tickets.findAll({
+    limit: 1,
+    order: [["createdAt", "DESC"]],
+  });
   let movieid = req.params.movieid;
   let showtimeid = req.params.showtimeid;
-  res.render("pages/submit");
+  res.render("pages/submit", {
+    latest: latest,
+  });
 });
 
 module.exports = router;
